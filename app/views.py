2366d709910a1ef628bb232abaa3a4c34eb2d109
Fred Sheppard <frederickjssheppard@gmail.com>
2024-02-28 05:01:03 +0000

    Fix bug stopping joining new clubs

--- app/views.py
+++ app/views.py
@@ -300,12 +300,7 @@ def view_club(request, club_id):
     is_member = ClubMembers.objects.filter(user=request.user, club=club).exists()
     already_requested = ClubRequests.objects.filter(user=request.user, club=club).exists()
     members = ClubMembers.objects.filter(club=club)
-    user_memberships = ClubMembers.objects.filter(user=request.user)
-    num_clubs_joined = user_memberships.count()
-    if num_clubs_joined < 3:
-        can_join_more_clubs = False
-    else:
-        can_join_more_clubs = True
+    can_join_more_clubs = ClubMembers.objects.filter(user=request.user).count() < 3
 
     context = {
         'club': club,
@@ -314,7 +309,6 @@ def view_club(request, club_id):
         'members': members,
         'coordinator': coordinator,
         'can_join_more_clubs': can_join_more_clubs,
-        'num_clubs_joined': num_clubs_joined,
     }
 
     return render(request, 'view_club.html', context)
