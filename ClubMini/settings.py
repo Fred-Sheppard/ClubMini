47f6c6d46e4790b8cc7343a76c28c2fad83fc360
Fred Sheppard <frederickjssheppard@gmail.com>
2024-02-28 04:50:15 +0000

    Actually fix it

--- ClubMini/settings.py
+++ ClubMini/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6%-pokp@w-!^-o)y$()ec^w0^9ovr26^4_!2npda@an#53%*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']  # Add other domains as needed
 
