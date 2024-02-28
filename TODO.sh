2465b188b81bfc6918d1804d24c171bdfa389bca
Fred Sheppard <frederickjssheppard@gmail.com>
2024-02-28 05:19:30 +0000

    Delete TODO.sh

--- TODO.sh
+++
@@ -1,9 +0,0 @@
-Write up DML and Triggers
-Rewrite DML to match models
-#Allow Admin to register himself (first user)
-#All pages login required
-Update profile info
-#Global events
-Profile links
-A Few views
-Hosting/Container
